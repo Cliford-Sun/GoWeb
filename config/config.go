6add630a8f1058b55a706775b61fe1bb0bc3ee0b
sunzc <128234414+Cliford-Sun@users.noreply.github.com>
2024-06-15 16:39:00 +0800

    连接gorm

--- config/config.go
+++ config/config.go
@@ -5,26 +5,3 @@ type Config struct {
 	Logger Logger `json:"logger"`
 	System System `json:"system"`
 }
-
-type Mysql struct {
-	Host      string `json:"host"`
-	Port      int    `json:"port"`
-	Db        string `json:"db"`
-	User      string `json:"user"`
-	Password  string `json:"password"`
-	Log_level string `json:"log_level"`
-}
-
-type Logger struct {
-	Level        string `json:"level"`
-	Prefix       string `json:"prefix"`
-	Director     string `json:"director"`
-	ShowLine     bool   `json:"show-line"`
-	LogInConsole bool   `json:"log-in-console"`
-}
-
-type System struct {
-	Host string `json:"host"`
-	Port int    `json:"port"`
-	Env  string `json:"env"`
-}
--- main.go
+++ main.go
@@ -1,1 +1,13 @@
-package GoWeb
+package main
+
+import (
+	"GoWeb/core"
+	"GoWeb/global"
+)
+
+func main() {
+	// 读取配置文件
+	core.InitConf()
+	// 连接数据库
+	global.DB = core.InitGorm()
+}
