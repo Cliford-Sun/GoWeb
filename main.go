3da57a7aa59b3c0e5745793ff119574a54c4e7dd
sunzc <128234414+Cliford-Sun@users.noreply.github.com>
2024-06-15 17:36:12 +0800

    logrus配置 logrus配置

--- main.go
+++ main.go
@@ -8,6 +8,11 @@ import (
 func main() {
 	// 读取配置文件
 	core.InitConf()
+	//初始化日志
+	global.Log = core.InitLogger()
+	global.Log.Warnln("111")
+	global.Log.Errorln("111")
+	global.Log.Infoln("111")
 	// 连接数据库
 	global.DB = core.InitGorm()
 }
